918212e96980e78ea875a2d26f0f94d7640178fa
Eric Anderson <eanderson@avinetworks.com>
2018-11-11 12:32:07 -0800

    updated test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,11 +5,9 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
 
-@pytest.mark.parametrize('pkg', [
-  'kubectl'
-])
-def test_pkg(host, pkg):
-    package = host.package(pkg)
-
-    assert package.is_installed
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
